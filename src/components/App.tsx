10191a7c1ec44475d1d23f29b55f4d0836b210f6
cocacola-lover <pegas620570@gmail.com>
2023-03-02 17:34:01 +0300

    Moved App

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './css/App.css';
+import PlayGrid from './PlayGrid';
+
+function App() {
+  return (
+    <div className="App">
+      <PlayGrid></PlayGrid>
+    </div>
+  );
+}
+
+export default App;
