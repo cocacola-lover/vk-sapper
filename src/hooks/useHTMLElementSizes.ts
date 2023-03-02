9effc90f0add6848534e25d94386897061584b1a
cocacola-lover <pegas620570@gmail.com>
2023-03-02 22:32:51 +0300

    Added hook to keep track of size

---
+++ src/hooks/useHTMLElementSizes.ts
@@ -0,0 +1,38 @@
+import { useState, useLayoutEffect } from "react";
+
+// Updates Sizes on windowResize Event
+
+export interface HTMLElementSizes {
+    x : number,
+    y : number, 
+    height : number, 
+    width : number
+}
+
+export default function useHTMLElementSizes (ref : React.RefObject<HTMLElement>) {
+    const [sizes, setSizes] = useState<HTMLElementSizes>({x : 0, y : 0, height : 0, width : 0});
+
+    useLayoutEffect(() => {
+        const element = ref.current;
+
+        function updateSizes () {
+            if (element === null) return;
+
+            setSizes({
+                y : element.getBoundingClientRect().left,
+                x : element.getBoundingClientRect().top,
+                width : element.offsetWidth,
+                height : element.offsetHeight,
+            })
+        }
+
+        window.addEventListener('resize', updateSizes);
+        updateSizes();
+
+        return () => {
+            window.removeEventListener('resize', updateSizes);
+        }
+    }, [ref]);
+
+    return sizes;
+}
