4fcaf3b9d0f5446419f848925e8c96261ace3bc3
cocacola-lover <pegas620570@gmail.com>
2023-03-03 22:25:53 +0300

    Added upperBar component

---
+++ src/components/UpperBar.tsx
@@ -0,0 +1,22 @@
+import './css/UpperBar.css';
+
+import Smile from './Smile';
+import { GameState } from './App';
+import ThreeDigitDisplay from './ThreeDigitDisplay';
+
+export interface UpperBarProps {
+    gameState : GameState,
+    setGameState : React.Dispatch<React.SetStateAction<GameState>>
+
+    timer : number,
+    flagsLeft : number
+}
+
+export default function UpperBar ({gameState, setGameState, timer, flagsLeft} : UpperBarProps) {
+
+    return <div className="UpperBar">
+        <ThreeDigitDisplay value={flagsLeft}/>
+        <Smile gameState={gameState} setGameState={setGameState}></Smile>
+        <ThreeDigitDisplay value={timer}/>
+    </div>
+}
